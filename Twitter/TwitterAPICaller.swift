2390e92a98c25268cbdd02e9a884b981347855c9
gyaltsentenzin <tenzingyaltsen15@gmail.com>
2022-03-11 17:59:53 -0600

    login feature added

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "jjKl3VkzisKAnLHFhxz4kD80R", consumerSecret: "ayq5m9rDZE0EZbmcLLv3PUJI1F04vvTBUpmNrHrQPVGhwBmVAn")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
---
+++ Twitter/loginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  loginViewController.swift
+//  Twitter
+//
+//  Created by Tenzin Gyaltsen on 3/11/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { error in
+            print("could not login")
+        })
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
